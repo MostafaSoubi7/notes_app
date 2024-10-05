4b3ffcfa023a9cbc9b8c3f2ab5ad156a676eb553
MostafaSoubi7 <mm0334471@gmail.com>
2024-10-06 02:36:02 +0300

    colors list view part two

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -48,9 +48,12 @@ class _AddNoteFormState extends State<AddNoteForm> {
             hint: "Content",
             maxLines: 5,
           ),
+          const SizedBox(
+            height: 32,
+          ),
           const ColorsListView(),
           const SizedBox(
-            height: 48,
+            height: 32,
           ),
 
           // const ColorItem(),
